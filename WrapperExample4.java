2b48476cfea5da8ec965c705b14415424c23f78a
Krishnapriya Sahoo <162333202+KrishnapriyaSahoo@users.noreply.github.com>
2025-05-27 22:09:37 +0530

    Add files via upload

---
+++ WrapperExample4.java
@@ -0,0 +1,10 @@
+public class  WrapperExample4
+{
+	public static void main(String[] args) 
+	{
+		Integer a=new Integer(12);
+		int i=a.intValue();
+		int j=a;
+		System.out.println(a+" "+i+" "+j);
+	}
+}

d820a91f1b485e0e4774f5697a14c15b87acb01b
Krishnapriya Sahoo <162333202+KrishnapriyaSahoo@users.noreply.github.com>
2025-05-27 22:06:55 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,13 @@
+class StringExample 
+{
+	public static void main(String[] args) 
+	{
+		String s1="Variable";
+		char ch[]={'a','r','r','a','y'};
+		String s2=new String(ch);
+		String s3=new String("string");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
